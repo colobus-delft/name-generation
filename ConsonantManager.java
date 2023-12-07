ba047dd4068584a598ee91af459bd408692310ce
Diego Fernandez <152980541+colobus-delft@users.noreply.github.com>
2023-12-07 12:19:18 +0700

    Create ConsonantManager.java

---
+++ ConsonantManager.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class ConsonantManager extends NamePart {
+    public ConsonantManager() {
+        this.elements = Arrays.asList("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z");
+    }
+}
